fd62e484c4066774a9f4fc9421e4c50d42644f1c
Tech Priest <petrus260@hotmail.com>
2025-04-03 12:24:31 -0300

    still starting things

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 const connectDB = require("./config/db");
 const express = require("express");
-
 const cors = require("cors");
 const { graphqlHTTP } = require("express-graphql");
-const graphqlCategory = require("../src/models/graphqlCategory");
-
+const graphqlCategory = require("../backend/models/graphqlCategory");
 require("dotenv").config();
-
 const app = express();
 // Connect to the database
 connectDB();
-
 // Middlewares
 app.use(cors());
 app.use(express.json());
-
 // API Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/orders", require("./routes/orderRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/cart", require("./routes/cartRoutes"));
 app.use("/api/category", require("./routes/categoryRoutes"));
-
 // GraphQL endpoint
 app.use(
   "/graphql",
@@ -30,7 +24,6 @@ app.use(
     graphiql: true, 
   })
 );
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server connectado na porta ${PORT}`);
