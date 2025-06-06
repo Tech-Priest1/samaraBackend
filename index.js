5b235baff9b341ec6285de3ffafbc1655a894c10
Tech Priest <petrus260@hotmail.com>
2025-06-06 18:11:27 -0300

    cors shenanegans

--- index.js
+++ index.js
@@ -12,11 +12,14 @@ const app = express();
 connectDB();
 
 // Middlewares
+app.options('*', cors());
+
 app.use(cors({
-  origin: '*', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'], 
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
 
 app.use(express.json());
 
