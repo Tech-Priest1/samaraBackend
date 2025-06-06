08b4f04302f558d5036a13251e4f8e55d7ba2fd1
Tech Priest <petrus260@hotmail.com>
2025-06-06 20:19:37 -0300

    back to port 80

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(
   })
 );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando em http://0.0.0.0:${PORT}`);
 });
